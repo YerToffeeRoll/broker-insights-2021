034f764d3d08e9172c1df14c80c6f36594be42c7
YerToffeeRoll <sam_killen_@hotmail.com>
2021-03-22 21:07:17 +0000

    feat: create the policy model with the relationship with the broker defined

---
+++ app/Models/Policy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Broker;
+
+class Policy extends Model
+{
+    use HasFactory;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'customer_name', 'customer_address', 'premium', 'policy_type', 'insurer_name',
+    ];
+
+
+    
+    /**
+     * Create the relationship with the broker.
+     *
+     * @var Broker 
+     */
+    public function broker()
+    {
+      return $this->belongsTo(Broker::class);
+    }
+}
